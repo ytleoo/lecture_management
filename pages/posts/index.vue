4847b6fd6465f34352447dca587eab6c5889d78f
Saya Kubota <3yata.k.ar@gmail.com>
2024-02-23 20:08:49 +0900

    :sparkles: ローカルAPi疎通テスト

---
+++ pages/posts/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const { data, pending, error, refresh } = await useFetch('http://localhost:3000/posts')
+</script>
+<template>
+  <section>
+    <TestComponent />
+    <ul>
+      <li v-for="d in data" :key="d.id">
+        {{ d.body }} / {{ d.created_at }}
+      </li>
+    </ul>
+  </section>
+</template>
--- pages/test/index.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <section>
-    <TestComponent />
-    <p>This page will be displayed at the /about route.</p>
-  </section>
-</template>
