958f5b159a9993f9620f457f4d5bb25e07c4aa8f
Saya Kubota <3yata.k.ar@gmail.com>
2024-02-23 17:14:00 +0900

    initial

---
+++ components/testComponent.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>test component</h1>
+</template>
---
+++ pages/test/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <section>
+    <TestComponent />
+    <p>This page will be displayed at the /about route.</p>
+  </section>
+</template>
