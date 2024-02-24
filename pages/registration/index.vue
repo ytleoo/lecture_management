fa3e692e685f13cc181e0ecb31e1e39648b3a51a
Saya Kubota <3yata.k.ar@gmail.com>
2024-02-24 15:40:52 +0900

    feat: 講義一覧取得

---
+++ pages/registration/index.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+const { data: lectures } = await useApi('/v1/public/lectures');
+</script>
+<template>
+  <section>
+    <ul>
+      <li v-for="lecture in lectures" :key="lecture.id">講義名：{{ lecture.name }}</li>
+    </ul>
+  </section>
+</template>
