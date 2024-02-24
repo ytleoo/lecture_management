464dca5ca8ea356296d2221cc060bd27abd216c9
Saya Kubota <3yata.k.ar@gmail.com>
2024-02-24 16:36:16 +0900

    feat: ヘッダーのデザイン

--- components/Common/HeaderMenu.vue
+++ components/Common/HeaderMenu.vue
@@ -1,17 +1,17 @@
 <script setup lang="ts">
 import { Menu, MenuButton, MenuItems, MenuItem } from '@headlessui/vue'
-import { Bars3Icon } from '@heroicons/vue/24/solid'
+import { Bars3Icon, Cog8ToothIcon } from '@heroicons/vue/24/solid'
 </script>
 <template>
   <div class="w-56 text-right">
     <Menu as="div" class="relative inline-block text-left">
       <div>
         <MenuButton
-          class="inline-flex w-full justify-center rounded-md bg-black/20 px-4 py-2 text-sm font-medium text-white hover:bg-black/30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75"
+          class="inline-flex w-full justify-center rounded-md bg-white bg-opacity-20 px-4 py-2 text-sm font-medium text-white hover:bg-white/30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75"
         >
           Menu
           <Bars3Icon
-            class="-mr-1 ml-2 h-5 w-5 text-violet-200 hover:text-violet-100"
+            class="-mr-1 ml-2 h-5 w-5"
             aria-hidden="true"
           />
         </MenuButton>
@@ -26,22 +26,30 @@ import { Bars3Icon } from '@heroicons/vue/24/solid'
         leave-to-class="transform scale-95 opacity-0"
       >
         <MenuItems
-          class="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none"
+          class="absolute right-0 mt-2 w-40 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none"
         >
           <div class="px-1 py-1">
             <MenuItem v-slot="{ active }">
               <button
                 :class="[
-                  active ? 'bg-violet-500 text-white' : 'text-gray-900',
+                  active ? 'bg-cyan-500 text-white' : 'text-gray-900',
                   'group flex w-full items-center rounded-md px-2 py-2 text-sm',
                 ]"
               >
-                <DeleteIcon
-                  :active="active"
-                  class="mr-2 h-5 w-5 text-violet-400"
-                  aria-hidden="true"
-                />
-                Delete
+                ログアウト
+              </button>
+            </MenuItem>
+            <MenuItem v-slot="{ active }">
+              <button
+                :class="[
+                  active ? 'bg-cyan-500 text-white' : 'text-gray-900',
+                  'group flex w-full items-center rounded-md px-2 py-2 text-sm',
+                ]"
+              >
+                <Cog8ToothIcon 
+                  class="mr-1 h-5 w-5"
+                  aria-hidden="true" />
+                パスワード変更
               </button>
             </MenuItem>
           </div>
