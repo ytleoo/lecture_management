1de613cc19b4e008209fd65de6965100ffc89ce6
Saya Kubota <3yata.k.ar@gmail.com>
2024-02-24 15:55:33 +0900

    feat: uiライブラリ導入

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,9 @@
+<script setup lang="ts">
+</script>
 <template>
-  <header class="bg-slate-500">
-    <h1 class="font-bold text-xl">Header</h1>
+  <header class="bg-slate-500 flex">
+    <h1 class="font-bold text-xl">講義管理システム</h1>
+    <CommonHeaderMenu />
   </header>
   <slot />
 </template>
