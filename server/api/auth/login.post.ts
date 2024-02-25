20d4083eaed25c6e8ae998ce4a7f0195dda0cf09
Saya Kubota <3yata.k.ar@gmail.com>
2024-02-25 19:48:30 +0900

    feat: zodバリデーション

--- server/api/auth/login.post.ts
+++ server/api/auth/login.post.ts
@@ -1,8 +1,28 @@
+import z from 'zod';
 import jwt from 'jsonwebtoken';
 
 export const SECRET = 'dummy';
 
 export default eventHandler(async (event) => {
+  const result = z
+    .object({ email: z.string().email(), password: z.string().min(1) })
+    .safeParse(await readBody(event));
+  if (!result.success) {
+    let message;
+    switch (result.error.issues[0].validation) {
+      case 'email':
+        message = 'メールアドレスが不正です';
+        break;
+      case 'password':
+        message = 'パスワードが不正です';
+        break;
+    }
+
+    throw createError({
+      statusCode: 403,
+      statusMessage: message,
+    });
+  }
 
   const expiresIn = 15;
   const { email } = result.data;
