88b13dc59cde6ac5bd2210f0eb0416f4c6726a94
Saya Kubota <3yata.k.ar@gmail.com>
2024-02-26 00:11:43 +0900

    refactor: サーバーサイドのバリデーション削除

--- server/api/auth/login.post.ts
+++ server/api/auth/login.post.ts
@@ -1,31 +1,10 @@
-import z from 'zod';
 import jwt from 'jsonwebtoken';
 
 export const SECRET = 'dummy';
 
 export default eventHandler(async (event) => {
-  const result = z
-    .object({ email: z.string().email(), password: z.string().min(1) })
-    .safeParse(await readBody(event));
-  if (!result.success) {
-    let message;
-    switch (result.error.issues[0].validation) {
-      case 'email':
-        message = 'メールアドレスが不正です';
-        break;
-      case 'password':
-        message = 'パスワードが不正です';
-        break;
-    }
-
-    throw createError({
-      statusCode: 403,
-      statusMessage: message,
-    });
-  }
-
+  const { email } = await readBody(event);
   const expiresIn = 15;
-  const { email } = result.data;
   const user = {
     email,
   };
