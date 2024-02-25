19cadc2ed3eab29813149c427bd5f9a126ca067b
Saya Kubota <3yata.k.ar@gmail.com>
2024-02-25 21:24:43 +0900

    feat: 新規登録ページの作成

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,2 @@
-<script setup lang="ts">
-import { definePageMeta } from '#imports';
-import { navigateTo } from '#app';
-
-definePageMeta({ auth: false });
-
-const handleLogin = async () => {
-  await navigateTo('/login');
-};
-</script>
-<template>
-  <div class="page-wrapper">
-    <div class="wrapper-white flex flex-col items-center gap-2">
-      <button class="button-white w-28" @click="handleLogin">ログイン</button>
-      <button class="text-sm text-gray-400 underline">ユーザー登録</button>
-    </div>
-  </div>
-</template>
+<script setup lang="ts"></script>
+<template></template>
--- pages/signup.vue
+++ pages/signup.vue
@@ -13,15 +13,19 @@ const { signIn } = useAuth();
 
 const email = ref('');
 const password = ref('');
+const passwordConfirm = ref('');
+
+
+const signUp = (email: string, password:string, passwordConfirm:string) => {
+  // signIn({ email, password }, { external: true, callbackUrl: '/registration' });
+}
 </script>
 <template>
   <div class="page-wrapper">
     <div class="wrapper-white">
-      <h1 class="text-xl font-bold">ログイン</h1>
-      <form
-        @submit.prevent="
-          signIn({ email, password }, { external: true, callbackUrl: '/registration' })
-        "
+      <h1 class="text-xl font-bold">ユーザー登録</h1>
+      <form 
+        @submit.prevent="signUp(email, password, passwordConfirm)"
         class="mt-4"
       >
         <div class="my-4 flex w-full flex-col items-center">
@@ -46,11 +50,22 @@ const password = ref('');
             class="w-full border-b px-4 py-2 focus:border-b-2 focus:border-cyan-500 focus:outline-none md:w-5/6"
           />
         </div>
+        <div class="my-4 flex w-full flex-col items-center">
+          <label for="password" class="w-full px-4 text-left text-gray-500 md:w-5/6"
+            >パスワード再入力</label
+          >
+          <input
+            v-model="passwordConfirm"
+            type="text"
+            placeholder="password"
+            id="passwordConfirm"
+            class="w-full border-b px-4 py-2 focus:border-b-2 focus:border-cyan-500 focus:outline-none md:w-5/6"
+          />
+        </div>
         <button type="submit" class="button-white w-28 bg-cyan-500 text-white hover:bg-cyan-400">
-          ログイン
+          登録
         </button>
       </form>
-      <button class="text-sm text-gray-400 underline mt-3" @click="navigateTo('/signup')">ユーザー新規登録</button>
     </div>
   </div>
 </template>
