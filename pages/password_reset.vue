8d31adbb3c5eadaad1ed309ff1bb175273414c7f
Saya Kubota <3yata.k.ar@gmail.com>
2024-02-29 21:06:00 +0900

    feat: パスワード変更画面

--- pages/password_reset.vue
+++ pages/password_reset.vue
@@ -1,30 +1,19 @@
 <script setup lang="ts">
-import { useAuth, definePageMeta } from '#imports';
 import { ref } from 'vue';
 import z from 'zod';
 import { useApi } from '~/composables/useApi';
 import { useApiError } from '~/composables/useApiError';
 import { useValidation } from '~/composables/useValidation';
 
-definePageMeta({
-  auth: {
-    unauthenticatedOnly: true,
-    navigateAuthenticatedTo: '/registration',
-  },
-});
-
-const { signIn } = useAuth();
 
 const errorMessage = ref<string | undefined>(undefined);
-const email = ref('');
 const password = ref('');
 const passwordConfirm = ref('');
 
 const { validateForm } = useValidation();
-const validationSignUp = (email: string, password: string, passwordConfirm: string) => {
+const validationPassword = (password: string, passwordConfirm: string) => {
   const formParser = z
     .object({
-      email: z.string().email({ message: 'メールアドレスが不正です' }),
       password: z
         .string()
         .min(6, { message: 'パスワードを6文字以上で入力してください' })
@@ -43,39 +32,39 @@ const validationSignUp = (email: string, password: string, passwordConfirm: stri
         });
       }
     });
-  errorMessage.value = validateForm(formParser, { email, password, passwordConfirm });
+  errorMessage.value = validateForm(formParser, { password, passwordConfirm });
 };
 
-const signUp = async (email: string, password: string, passwordConfirm: string) => {
+const resetPassword = async (password: string, passwordConfirm: string) => {
   errorMessage.value = '';
-  validationSignUp(email, password, passwordConfirm);
+  validationPassword(password, passwordConfirm);
   if (errorMessage.value) return;
 
-  const { error } = await useApi('api/v1/auth', {
-    httpMethod: 'POST',
-    params: { email: email, password: password },
+  const { error } = await useApi('api/v1/auth/password', {
+    httpMethod: 'PUT',
+    params: { password: password, password_confirmation: passwordConfirm },
   });
   if (error.value) {
     const errors = useApiError(error)
     errorMessage.value = errors?.full_messages?.[0] ?? '登録失敗';
     return;
   }
-  signIn({ email, password }, { external: true, callbackUrl: '/registration' });
+
+  alert('変更しました')
 };
 </script>
 <template>
   <div class="page-wrapper">
     <div class="wrapper-white">
-      <h1 class="text-xl font-bold">ユーザー登録</h1>
+      <h1 class="text-xl font-bold">パスワード変更</h1>
       <CommonFlashMessage type="error" :isVisible="!!errorMessage">{{
         errorMessage
       }}</CommonFlashMessage>
-      <form @submit.prevent="signUp(email, password, passwordConfirm)" class="mt-4">
-        <CommonFormInput type="text" id="email" placeholder="email@email" label="メールアドレス" v-model:modelValue="email" />
-        <CommonFormInput type="text" id="password" placeholder="password" label="パスワード" v-model:modelValue="password" />
+      <form @submit.prevent="resetPassword(password, passwordConfirm)" class="mt-4">
+        <CommonFormInput type="text" id="password" placeholder="password" label="新しいパスワード" v-model:modelValue="password" />
         <CommonFormInput type="text" id="passwordConfirm" placeholder="password" label="パスワード再入力" v-model:modelValue="passwordConfirm" />
         <button type="submit" class="button-base w-28 bg-cyan-500 text-white hover:bg-cyan-400">
-          登録
+          変更
         </button>
       </form>
     </div>
