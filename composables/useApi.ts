836f3ed09c74615e3a1fdc62344a67d9e3c5552a
Saya Kubota <3yata.k.ar@gmail.com>
2024-03-03 13:50:26 +0900

    feat: authヘッダをつけてリクエストできるようにする

--- composables/useApi.ts
+++ composables/useApi.ts
@@ -1,12 +1,5 @@
 import { useFetch, useRuntimeConfig } from '#app';
 
-const buildWrightHeaders = () => {
-  const headers = {
-    'Content-Type': 'application/json',
-  };
-  return headers;
-};
-
 type Params<T extends string | number> = {
   [key: string]: T;
 };
@@ -14,20 +7,36 @@ type Params<T extends string | number> = {
 type Options = {
   httpMethod?: 'GET' | 'POST' | 'PATCH' | 'PUT' | 'DELETE';
   params?: Params<string | number>;
-  baseURL?: string;
 };
 
-// header付与もする
-export const useApi = (path: string, { httpMethod = 'GET', params }: Options = {}) => {
+type ReqOptions = Options & {
+  baseURL: string;
+  timeout: number;
+};
+
+export const useApi = async (path: string, { httpMethod = 'GET', params }: Options = {}) => {
   const config = useRuntimeConfig();
-  const options: Options = { baseURL: config.public.baseURL };
+  const options: ReqOptions = { baseURL: config.public.baseURL, timeout: 3000 };
+
+  const { data } = await useFetch('/api/token');
+  const authHeaders = data.value?.authHeaders ?? '';
+  if (authHeaders) {
+    Object.assign(options, {
+      timeout: 3000,
+      headers: {
+        uid: authHeaders.uid,
+        client: authHeaders.client,
+        'access-token': authHeaders.accessToken,
+      },
+    });
+  }
+
   switch (httpMethod) {
     case 'POST':
     case 'PATCH':
     case 'PUT':
     case 'DELETE':
       Object.assign(options, {
-        headers: buildWrightHeaders(),
         method: httpMethod,
         ...(params ? { body: { ...params } } : {}),
       });
@@ -35,8 +44,6 @@ export const useApi = (path: string, { httpMethod = 'GET', params }: Options = {
     default:
       Object.assign(options, { method: 'GET', ...(params ? { query: { ...params } } : {}) });
   }
-
-  Object.assign(options, { timeout: 3000 });
 
   return useFetch(path, options);
 };
--- server/api/auth/login.post.ts
+++ server/api/auth/login.post.ts
@@ -6,15 +6,15 @@ export default eventHandler(async (event) => {
   const SECRET = 'secret';
   const expiresIn = 60 * 60 * 24 * 1 * 1000;
 
-  const token = jwt.sign(authHeaders, SECRET, {
+  const token = jwt.sign(authHeaders._value, SECRET, {
     expiresIn,
   });
 
   const options = {
     maxAge: expiresIn,
     httpOnly: true,
-  }
-  setCookie(event, "token", token, options)
+  };
+  setCookie(event, 'token', token, options);
 
   const user = { uid };
   const accessToken = jwt.sign(user, SECRET, {
